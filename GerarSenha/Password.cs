16dced70ce244cfb54883f14b5a01e0d741c17d4
Otávio Bernardo <99296822+otaviobernard@users.noreply.github.com>
2022-08-31 19:26:32 -0300

    Add files via upload

---
+++ GerarSenha/Password.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerarSenha
+{
+    internal class Password
+    {
+
+        private string _password;
+        public string Senha
+        {
+            get
+            {
+                return this._password;
+            }
+            set
+            {
+                this._password = value;
+            }
+        }
+
+        public string passwordGenerator(int size)
+        {
+            Random random = new Random();
+            int aux = 0;
+            string padrao = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890987654321_?!";
+            string senha = "";
+            for (int i = 0; i < size; i++)
+            {
+                aux = random.Next(0, 73);
+                senha = padrao[aux] + senha;
+            }
+            return senha;
+        }
+    }
+}
---
+++ GerarSenha/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerarSenha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tamanhoSenha;
+            Password senha = new Password();
+            Console.WriteLine("Informe o tamanho da senha");
+            tamanhoSenha = int.Parse(Console.ReadLine());
+            senha.Senha = senha.passwordGenerator(tamanhoSenha);
+            Console.WriteLine(senha.Senha);
+        }
+    }
+}
